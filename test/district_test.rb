995aa853cad676637617e8b7255ab0d66b6959ff
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-01-04 11:07:57 -0700

    adds district name

--- test/district_test.rb
+++ test/district_test.rb
@@ -1,1 +1,10 @@
 require_relative "test_helper"
+require "./lib/district"
+
+class DistrictTest < Minitest::Test
+
+  def test_does_district_it_have_a_name
+    d = District.new(:name => "ACADEMY 20")
+    assert_equal "ACADEMY 20", d.name
+  end
+end
