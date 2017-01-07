276afba94aee0eae5ab435c1a5844a5d86be7a60
Katy Farmer <dreadpiratekaty@gmail.com>
2017-01-06 20:04:04 -0700

    Adds kindergarten rate trends and finishes iteration one

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -15,15 +15,28 @@ class HeadcountAnalyst
   end
 
   def kindergarten_participation_rate_variation(name_1, name_2)
-    truncates_float((find_district_data(name_1) /
-    find_district_data(name_2[:against])))
+    truncates_float((find_district_data_average(name_1) /
+    find_district_data_average(name_2[:against])))
+  end
 
+  def kindergarten_participation_rate_variation_trend(name_1, name_2)
+    data_1 = find_district_data(name_1)
+    data_2 = find_district_data(name_2[:against])
+    trends = {}
+    data_1.each do |key, value|
+      trends[key] = truncates_float(data_1[key] / data_2[key])
+    end
+    trends
+  end
+
+  def find_district_data_average(name)
+    average(find_district_data(name).values)
   end
 
   def find_district_data(name)
     district = @dr.find_by_name(name)
     enrollment = district.enrollment
     district_numbers = enrollment.kindergarten_participation_by_year
-    average(district_numbers.values)
   end
+
 end
